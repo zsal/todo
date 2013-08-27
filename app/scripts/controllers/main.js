9dd701da929dc5de5d0b7036004fe4d4bb16f73c
Zane Salem <zsalem@umich.edu>
2013-08-26 22:30:47 -0400

    activated local storage of todo items

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,15 +1,15 @@
 'use strict';
 
 angular.module('todoApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, localStorageService) {
 
-    $scope.todos = ['Item1', 'Item2', 'Item3'];
+    // $scope.todos = ['Item1', 'Item2', 'Item3'];
 
-    // var todosInStore = localStorageService.get('todos');
-    // $scope.todos = todosInStore && todosInStore.split('\n') || [];
-    // $scope.$watch(function () {
-    //     localStorageService.add('todos', $scope.todos.join('\n'));
-    //   });
+    var todosInStore = localStorageService.get('todos');
+    $scope.todos = todosInStore && todosInStore.split('\n') || [];
+    $scope.$watch(function () {
+        localStorageService.add('todos', $scope.todos.join('\n'));
+      });
 
     $scope.addTodo = function () {
         $scope.todos.push($scope.todo);
